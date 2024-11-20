c62a41857f31641b75207514514b826586e76e5d
AliAttia635 <aliattya14@gmail.com>
2024-11-20 19:37:05 +0200

    change to dark theme and add animation to splash screen

--- lib/features/presentation/views/widgets/splash_view_body.dart
+++ lib/features/presentation/views/widgets/splash_view_body.dart
@@ -1,28 +1,58 @@
 import 'package:bookly_app/core/constants/assets/assets_images.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SplashViewBody extends StatelessWidget {
+import 'sliding_text.dart';
+
+class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    initAnimationSliding();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    animationController.dispose();
+  }
+
+  void initAnimationSliding() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 3), end: const Offset(0, 0))
+            .animate(animationController);
+
+    animationController.forward();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
-      // crossAxisAlignment: CrossAxisAlignment.stretch,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Image.asset(AssetsImages.logoImage),
         const SizedBox(
-          height: 25,
+          height: 4,
         ),
-        const Center(
-          child: Text(
-            "Read Books for free",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-            ),
-          ),
-        ),
+        SlidingText(slidingAnimation: slidingAnimation),
       ],
     );
   }
