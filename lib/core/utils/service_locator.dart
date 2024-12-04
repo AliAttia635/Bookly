a8416cf36be408c176323f9c28650b0a188ebfc0
AliAttia635 <aliattya14@gmail.com>
2024-12-04 06:07:59 +0200

    implement Dependency injection and setup service locator by using getIt package

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,12 @@
+import 'package:bookly_app/core/utils/api_service.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_implementaion.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getit = GetIt.instance;
+
+void setupServiceLocator() {
+  getit.registerSingleton<HomeRepoImplementaion>(
+      HomeRepoImplementaion(getit.get<ApiService>()));
+  getit.registerSingleton<ApiService>(ApiService(Dio()));
+}
