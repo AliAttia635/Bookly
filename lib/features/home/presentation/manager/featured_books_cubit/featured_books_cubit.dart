f77d9845467a393096517f7fa5d96422868f13e4
AliAttia635 <aliattya14@gmail.com>
2024-12-04 03:02:43 +0200

    create getFeaturedBooks method in cubit and used fold builtIn function in it

--- lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart
@@ -1,9 +1,26 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/core/errors/failures.dart';
 import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
+import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  HomeRepo homeRepo;
+
+  Future<void> getFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+
+    Either<Failure, List<BookModel>> result =
+        await homeRepo!.fetchFeaturedBooks();
+    result.fold((failue) {
+      emit(FeaturedBooksFailure(errMessage: failue.errMessage));
+    }, (booksList) {
+      emit(FeaturedBooksSuccess(booksList: booksList));
+    });
+  }
 }
